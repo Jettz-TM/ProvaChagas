5236a245f43e21e8fec7af3204b15133f2624b37
Roney <miltonroneynobre@gmail.com>
2024-09-16 15:51:35 -0300

    correção de bugs no sistema de combate e adição de um menu de itens provisório

--- CriaPersonagem/obj/Debug/net8.0/CriaPersonagem.AssemblyInfo.cs
+++ CriaPersonagem/obj/Debug/net8.0/CriaPersonagem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CriaPersonagem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a408b667d4984541e4e899a8604887cd1807117")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0da888b41de231075440ead41c8075a6954d2530")]
 [assembly: System.Reflection.AssemblyProductAttribute("CriaPersonagem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CriaPersonagem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
